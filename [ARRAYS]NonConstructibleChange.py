82afce142622c396899a1f1344736e4cc6eaec1a
Omkar Rajguru <72383143+orajguru@users.noreply.github.com>
2023-04-16 22:43:00 -0700

    Create [ARRAYS]NonConstructibleChange.py

---
+++ [ARRAYS]NonConstructibleChange.py
@@ -0,0 +1,8 @@
+def nonConstructibleChange(coins):
+    coins.sort();
+    change = 0;
+    for coin in coins:
+        if coin > change + 1:
+            break
+        change += coin
+    return change + 1
